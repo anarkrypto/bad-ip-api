0657743e7ab719252100f434ab1fee3b69e2ba57
anarkrypto <anarkrypto@gmail.com>
2023-09-22 23:28:08 -0300

    🔧 Add DNSBLs blacklists

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const blacklists = [
+    "dnsbl.dronebl.org",
+    "sbl.spamhaus.org",
+    "dnsbl-1.uceprotect.net",
+    "b.barracudacentral.org",
+    "spam.dnsbl.anonmails.de",
+    "dnsbl.sorbs.net",
+]
